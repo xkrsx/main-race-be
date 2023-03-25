cc0be25fd7772d77fe5a2a03ba3b72d7eb058c4e
Jakub Markiewicz <markiewicz267@gmail.com>
2023-03-25 06:49:57 +0100

    Moved Insert (new Job by Courier) to New-Job Record from Courier-View. It works now with FE.

--- records/courier-view.record.ts
+++ records/courier-view.record.ts
@@ -64,29 +64,10 @@ export class CourierViewRecord implements CourierViewEntity {
 
     static async getOne(courierNumber: number): Promise<any> {
         const [results] = await pool.execute(
-            "SELECT couriers.courierId, couriers.courierNumber, couriers.courierName, couriers.category, couriers.courierPoints, couriers.courierPenalties, jobs.jobId, jobs.jobNumber, jobs.cp_a_name, jobs.cp_a_code, jobs.cp_b_name, jobs.cp_b_code, jobs.cp_c_name, jobs.cp_c_code, jobs.jobPoints, couriers_jobs.jobPenalties, couriers_jobs.finished FROM couriers JOIN couriers_jobs ON couriers.courierNumber = couriers_jobs.courierNumber JOIN jobs ON couriers_jobs.jobNumber = jobs.jobNumber WHERE couriers.courierNumber = :courierNumber", {
+            "SELECT couriers.courierId, couriers.courierNumber, couriers.courierName, couriers.category, couriers.courierPoints, couriers.courierPenalties, jobs.jobId, jobs.jobNumber, jobs.cp_a_name, jobs.cp_a_code, jobs.cp_b_name, jobs.cp_b_code, jobs.cp_c_name, jobs.cp_c_code, jobs.jobPoints, couriers_jobs.jobPenalties, couriers_jobs.finished FROM couriers JOIN couriers_jobs ON couriers.courierNumber = couriers_jobs.courierNumber JOIN jobs ON couriers_jobs.jobNumber = jobs.jobNumber WHERE couriers.courierNumber = :courierNumber ORDER BY couriers_jobs.jobNumber ASC", {
                 courierNumber
             }) as CourierViewResults;
         return results.map(obj => new CourierViewRecord(obj));
-    }
-
-    async insert(courierNumber: number, jobNumber?: number): Promise<string> {
-        if (!this.id) {
-            this.id = uuid();
-        } else {
-            throw new Error("Cannot insert this job - it already exists.")
-        }
-
-        if (!this.jobNumber) {
-            this.jobNumber = codeGenerator(1, 10);
-        }
-
-        await pool.execute("INSERT INTO `couriers_jobs` (`id`, `courierNumber`, `jobNumber`, `pickup`, `dropoff`, `jobPenalties`, `finished`) VALUES (NULL, :courierNumber, :jobNumber, CURRENT_TIMESTAMP, NULL, NULL, NULL)", {
-            courierNumber,
-            jobNumber,
-        });
-
-        return this.id;
     }
 
 }
